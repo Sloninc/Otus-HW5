91dcfca4b08b233a6537159e1f570c0c7678c7c4
Andrei Ruk <sloninc@mail.ru>
2023-05-13 22:28:56 +0300

    В методе Main добавлена проверка превышения размера строки.

--- Program.cs
+++ Program.cs
@@ -36,58 +36,83 @@ namespace Class5
         /// <exception cref="ArgumentOutOfRangeException"></exception>
         public static void OutputStringTab(TypeStringTab typeString, int countTab, string inputString)
         {
-            try
+            //длинна строки таблицы
+            int _countString = 2 * countTab + inputString.Length;
+
+            //строка символов '+'
+            string? _strplus = new string('+', _countString - 2);
+
+            //циклический вывод элементов в строки таблицы
+            for (int i = 0; i <= _countString - 1; i++)
             {
-                int _countString = 2 * countTab + inputString.Length; //длинна строки таблицы
-                if (_countString > 40) //проверка на соответствии размерности таблицы
-                    throw new ArgumentOutOfRangeException("размерноть таблицы и текст", "строки в таблице не должны превышать 40 символов");
-                string? _strplus = new string('+', _countString - 2); //строка символов '+'
-                for (int i = 0; i <= _countString - 1; i++) //циклический вывод элементов в строки таблицы
+                switch (typeString)
                 {
-                    switch (typeString)
-                    {
-                        case TypeStringTab.firstString:
-                            if (i > 2 * countTab) break;  // проверка на выход из диапазона данной строки переменной цикла
-                            if (i == 0 | i == 2 * countTab)  
-                                Console.WriteLine(new String('+', _countString));// вывод границ первой строки таблицы
-                            else if (i == countTab)
-                                Console.WriteLine("+" + new string(' ', countTab - 1) + 
-                                    inputString + new string(' ', countTab - 1) + "+"); // вывод пользовательского текста первой строки таблицы
-                            else Console.WriteLine("+" + new string(' ', _countString - 2) + "+"); // вывод пустого пространства первой строки таблицы
-                            break;
-                        case TypeStringTab.secondString:
-                            if (i > 2 * countTab) break; // проверка на выход из диапазона данной строки переменной цикла
-                            var _blackChess = _strplus.ToList().Select((m, n) => (n % 2 == 0) ? m = ' ' : m = '+').ToArray(); // формируем строки для расстановки символа '+' в шахматном порядке
-                            var _whiteChess = _strplus.ToList().Select((m, n) => (n % 2 == 0) ? m = '+' : m = ' ').ToArray(); // формируем строки для расстановки символа '+' в шахматном порядке
-                            if (i % 2 == 0) Console.WriteLine("+" + new string(_blackChess) + "+");// расставляем символы '+' в шахматном порядке
-                            else Console.WriteLine("+" + new string(_whiteChess) + "+");// расставляем символы '+' в шахматном порядке
-                            break;
-                        case TypeStringTab.thirdString:
-                            if (i == 0 | i == _countString - 1)
-                                Console.WriteLine(new String('+', _countString));// вывод границ третьей строки таблицы
-                            else
-                            {
-                                var _diagonalplus = _strplus.ToList().Select((m, n) => (n == i - 1) | 
-                                (n == _countString - 2 - i) ? m = '+' : m = ' ').ToArray(); // формируем строки для расстановки символа '+' по диагонали
-                                Console.WriteLine("+" + new String(_diagonalplus) + "+"); // расставляем символы '+' в диагональном порядке
-                            }
-                            break;
-                    }
+                    case TypeStringTab.firstString:
+
+                        // проверка на выход из диапазона данной строки переменной цикла
+                        if (i > 2 * countTab) break;
+                        if (i == 0 | i == 2 * countTab)
+                            // вывод границ первой строки таблицы
+                            Console.WriteLine(new String('+', _countString));
+                        else if (i == countTab)
+                            // вывод пользовательского текста первой строки таблицы
+                            Console.WriteLine("+" + new string(' ', countTab - 1) +
+                                inputString + new string(' ', countTab - 1) + "+");
+                        else
+                            // вывод пустого пространства первой строки таблицы
+                            Console.WriteLine("+" + new string(' ', _countString - 2) + "+");
+                        break;
+
+                    case TypeStringTab.secondString:
+
+                        // проверка на выход из диапазона данной строки переменной цикла
+                        if (i > 2 * countTab) break;
+
+                        // формируем строки для расстановки символа '+' в шахматном порядке
+                        var _blackChess = _strplus.ToList().
+                            Select((m, n) => (n % 2 == 0) ? m = ' ' : m = '+').ToArray();
+
+                        // формируем строки для расстановки символа '+' в шахматном порядке
+                        var _whiteChess = _strplus.ToList().
+                            Select((m, n) => (n % 2 == 0) ? m = '+' : m = ' ').ToArray();
+                        if (i % 2 == 0)
+                            // расставляем символы '+' в шахматном порядке
+                            Console.WriteLine("+" + new string(_blackChess) + "+");
+                        else
+                            // расставляем символы '+' в шахматном порядке
+                            Console.WriteLine("+" + new string(_whiteChess) + "+");
+                        break;
+
+                    case TypeStringTab.thirdString:
+
+                        if (i == 0 | i == _countString - 1)
+                            // вывод границ третьей строки таблицы
+                            Console.WriteLine(new String('+', _countString));
+                        else
+                        {
+                            var _diagonalplus = _strplus.ToList().Select((m, n) => (n == i - 1) |
+
+                            // формируем строки для расстановки символа '+' по диагонали
+                            (n == _countString - 2 - i) ? m = '+' : m = ' ').ToArray();
+
+                            // расставляем символы '+' в диагональном порядке
+                            Console.WriteLine("+" + new String(_diagonalplus) + "+");
+                        }
+                        break;
                 }
             }
-            catch(ArgumentOutOfRangeException ex)
-            {
-                Console.WriteLine(ex.Message);
-                Main();
-            }
         }
+
         /// <summary>
         /// Точка входа. Получает от пользователя размерность таблицы и текст, далее вызывает метод вывода таблицы.
         /// </summary>
         static void Main()
         {
-            int n;  //переменная name="n" содержит размерность таблицы согласно заданию
-            while (true) // "бесконечный" цикл для получения от пользователя значения размерности в пределах диапазона.
+            //переменная name="n" содержит размерность таблицы согласно заданию
+            int n;
+
+            // "бесконечный" цикл для получения от пользователя значения размерности в пределах диапазона
+            while (true) 
             {
                 Console.WriteLine("Введите размерность таблицы:");
                 var inputTabCount = Console.ReadLine();
@@ -99,14 +124,24 @@ namespace Class5
                 }
                 else Console.WriteLine("Значение размерности должно быть целочисленным в диапазоне от 1 до 6");
             }
-        
-            var checkEmpty = false;
-            string? inputText; //переменная name="inputText" содержит текст, введенный пользователем
-            do // цикл с пост проверкой условия, что пользователь ввел текст
+            //переменная name="inputText" содержит текст, введенный пользователем
+            string? inputText;
+
+            var checkEmpty = true;
+            // цикл с пост проверкой условия, что пользователь ввел текст
+            do
             {
                 Console.WriteLine("Введите произвольный текст:");
                 inputText = Console.ReadLine();
                 checkEmpty = string.IsNullOrEmpty(inputText);
+                //длинна строки таблицы
+                int _countString = 2 * n + inputText.Length;
+                //проверка на соответствие размерности таблицы
+                if (_countString > 40)
+                {
+                    Console.WriteLine("Размер строки превышает 40 символов");
+                    checkEmpty = true;
+                }
             }
             while (checkEmpty);
 
